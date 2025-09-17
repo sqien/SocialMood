7bafa95e36470c494d2421772b4e9fc61c102e7c
sqien <twitchsqien@gmail.com>
2025-09-17 11:14:26 +0200

    Added tkinter GUI

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
 import pymysql.cursors
-
-def db_submit_rating():  
-    mood_list = ["Barely holding on","Worse than usual", "Meh", "Pretty good", "Good", "Perfect"]
-
-    for elements, mood in enumerate(mood_list, start=1):
-        print(f"{elements}: {mood}")
-        pass
-
-    mood_input = int(input("What's your mood today, choose from the list: "))
-    choosen_mood = mood_list[mood_input - 1]
-
-    while mood_input < 1 or mood_input > len(mood_list):
-        print("You only have 6 options, buddy))")
-        mood_input = int(input("What's your mood today, choose from the list: "))
-    print(f"You choose option: {mood_input}, which means your mood is: {choosen_mood}")
-    return choosen_mood
-
-def db_submit_comment():
-    comment_input = input("Enter your commentary: ")
-    return comment_input
+import tkinter as tk
 
 
-def main():
+def main(mood, comment):
     connection = pymysql.connect(host='localhost',
                                 user='root',
                                 password='',
                                 database='socialmood',
                                 cursorclass=pymysql.cursors.DictCursor)
     
-    mood = db_submit_rating()
-    comment = db_submit_comment()
-
     with connection:
         with connection.cursor() as cursor:
             sql = "INSERT INTO `mood_entries` (`rating`, `comment`) VALUES (%s, %s)"
@@ -43,4 +21,36 @@ def main():
             cursor.execute(sql)
             return cursor.fetchone()
         
-main()
+class Info():
+    def __init__(self):
+        self.mood_input = ""
+        self.comment_input = ""
+    def submit(self):
+        self.mood_input = radio_button.get()
+        self.comment_input = entry_comment.get()
+        print(self.mood_input, self.comment_input)
+        main(self.mood_input, self.comment_input)
+        
+root = tk.Tk()
+root.geometry("600x300")
+radio_button = tk.StringVar(root)
+entry_comment = tk.StringVar(root)
+moods = [
+    "Barely holding on",
+    "Worse than usual",
+    "Meh",
+    "Pretty good",
+    "Good",
+    "Perfect"
+]
+for m in moods:
+    tk.Radiobutton(root, text=m, variable=radio_button, value=m).pack(anchor="w")
+
+tk.Entry(root, textvariable=entry_comment).pack(fill="x", padx=8, pady=8)
+
+info = Info() 
+tk.Button(root, text="Submit", command=info.submit).pack(pady=8)
+tk.Button(root, text="Exit", command=quit).pack()
+
+
+root.mainloop()
